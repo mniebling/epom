dc1d405b50c6f4dabf8f7cda53542940e99bf01e
Mike Niebling <>
2018-10-04 20:00:08 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Esther Pomranky</h1>
+    <p>Chicago-based art educator &amp; artist.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'app',
+  components: {}
+})
+</script>
+
+<style lang="postcss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 60px 0 0 60px;
+}
+</style>
