9644696737a172cd2a2b06103ee067e5e3f039b0
Mike Niebling <>
2018-10-06 14:31:15 -0500

    Add images and forms link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
   <div id="app">
     <h1>Esther Pomranky</h1>
-    <p>Chicago-based art educator &amp; artist.</p>
+    <p>Chicago-based teaching artist.</p>
+
+    <div class="birds">
+      <div class="birds-images">
+        <div class="bird-image">
+          <img src="./assets/purple-finch.jpg">
+        </div>
+        <div class="bird-image">
+          <img src="./assets/cardinal.jpg">
+        </div>
+        <div class="bird-image">
+          <img src="./assets/titmouse.jpg">
+        </div>
+        <div class="bird-image">
+          <img src="./assets/bluebird.jpg">
+        </div>
+        <div class="bird-image">
+          <img src="./assets/goldfinch.jpg">
+        </div>
+        <div class="bird-image">
+          <img src="./assets/sparrow.jpg">
+        </div>
+      </div>
+      <div class="text">
+        <div>Interested in a print from the Birds series?</div>
+        <a href="https://goo.gl/forms/QhlJwFtLJwXri9q82">Leave your email</a>
+        <span> to be contacted when they're available.</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,5 +50,31 @@ export default Vue.extend({
   color: #2c3e50;
   margin-top: 60px;
   text-align: center;
+}
+
+.birds {
+  margin-top: 50px;
+}
+
+.birds-images {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  width: 75%;
+}
+
+.bird-image {
+  align-items: center;
+  display: flex;
+  margin: 0 5px;
+}
+
+.bird-image > img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.text {
+  margin-top: 50px;
 }
 </style>
