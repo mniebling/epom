1fcf7f365215279767c9dab03cac633e52f9a804
Mike Niebling <michael.niebling@gmail.com>
2024-09-01 21:51:04 -0500

    Fix fonts

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,17 +1,24 @@
 import Head from 'next/head'
+import { Source_Serif_4 } from 'next/font/google'
+
+
+const sourceSerif = Source_Serif_4({
+	display: 'swap',
+	subsets: ['latin'],
+	style: ['normal', 'italic'],
+})
 
 export default function Layout ({ children }) {
 
 	return (
 		<>
 			<Head>
-				<link rel="icon" href="/favicon.png" />
-				<link rel="apple-touch-icon" href="/favicon.png" />
-
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,400;0,700;1,400&display=swap" />
+				<link rel='icon' href='/favicon.png' />
+				<link rel='apple-touch-icon' href='/favicon.png' />
+				{/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,400;0,700;1,400" /> */}
 			</Head>
 
-			<main>{ children }</main>
+			<main className={ sourceSerif.className }>{ children }</main>
 		</>
 	)
 }
